e764b741abe6cd51d415481b285822b92844a478
Izabel <izabel.dantas@escolar.ifrn.edu.br>
2025-04-01 15:15:01 -0300

    styled components finalizado

---
+++ src/styles.js
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const Title = styled.h1`
+    color: #f00;
+    justify-content: center;
+`;
+
+export const Corpo = styled.div`
+    background-color: #000;
+`;
